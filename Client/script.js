96c245050bc440cc3295c0581599e015ffdf8776
Jakub Charvat <jakcharvat@gmail.com>
2021-07-03 02:49:45 +0200

    First version

--- Client/script.js
+++ Client/script.js
@@ -9,6 +9,52 @@ const input = document.getElementById('input')
 const sendButton = document.getElementById('sendButton')
 const sendIcon = sendButton.children[0]
 
+const newChatButton = document.getElementById('newChatButton')
+const joinChatButton = document.getElementById('joinChatButton')
+
+const newRoomPopup = document.getElementById('newRoomPopup')
+const joinRoomPopup = document.getElementById('joinRoomPopup')
+
+const createRoomForm = newRoomPopup.querySelector('form')
+const roomNameTf = document.getElementById('roomNameTf')
+
+const joinRoomForm = joinRoomPopup.querySelector('form')
+const roomCodeTf = document.getElementById('roomCodeTf')
+
+const chatsContainer = document.getElementById('chatsContainer')
+const roomTemplate = document.getElementById('roomTemplate')
+
+const chatSelectedView = document.getElementById('chatSelected')
+const chatNotSelectedView = document.getElementById('chatNotSelected')
+
+const namePopup = document.getElementById('namePopup')
+const nameForm = namePopup.querySelector('form')
+const usernameTf = document.getElementById('userNameTf')
+const enterButton = document.getElementById('enterButton')
+
+const channelNameLabel = document.getElementById('channelName')
+const channelCodeButton = document.getElementById('channelCode')
+const channelCodeSpan = channelCodeButton.querySelector('span')
+const channelCodeTooltip = channelCodeButton.querySelector('.tooltip')
+
+const userJoinedTemplate = document.getElementById('userJoinedTemplate')
+const userLeftTemplate = document.getElementById('userLeftTemplate')
+const chatMessageTemplate = document.getElementById('chatMessageTemplate')
+
+
+const popups = document.querySelectorAll('.popup-wrapper')
+popups.forEach(popup => {
+    const closeButton = popup.querySelector('.cancel-button')
+    if (closeButton) {
+        closeButton.onclick = () => {
+            closePopup(popup)
+        }
+    }
+})
+
+let activeButton
+
+
 form.addEventListener('submit', e => {
     e.preventDefault()
 
@@ -20,12 +66,147 @@ form.addEventListener('submit', e => {
     }
 })
 
-socket.on('chat message', msg => {
-    const item = document.createElement('li')
-    item.textContent = msg
-    messages.appendChild(item)
-    window.scrollTo(0, document.body.scrollHeight)
+
+nameForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    socket.emit('register', usernameTf.value)
+    closePopup(namePopup)
 })
+
+
+socket.on('chat message', msg => {
+    showMessage(msg)
+})
+
+socket.on('room joined', code => {
+    const split = code.split('#')
+    const roomCode = split[0]
+    split.splice(0, 1)
+    const roomName = split.join('#')
+
+    const newRoom = roomTemplate.content.cloneNode(true)
+    const button = newRoom.querySelector('.room')
+    const name = newRoom.querySelector('.name')
+    const codeSpan = newRoom.querySelector('.code-span')
+
+    name.innerText = roomName
+    codeSpan.innerText = roomCode
+
+    chatsContainer.appendChild(newRoom)
+    
+    button.addEventListener('click', () => {
+        openRoom(button, roomCode)
+    })
+
+    openRoom(button, roomCode)
+})
+
+socket.on('opened room', data => {
+    channelNameLabel.innerText = data.roomName
+    channelCodeSpan.innerText = data.roomCode
+
+    channelCodeButton.onclick = async () => {
+        try {
+            await navigator.clipboard.writeText(data.roomCode)
+            channelCodeTooltip.classList.remove('animate')
+            setTimeout(() => channelCodeTooltip.classList.add('animate'), 50)
+        } catch(error) {
+            alert(`Error copying to clipboard: ${error}`)
+        }
+    }
+
+    socket.emit('get chat history', data.roomCode)
+})
+
+socket.on('chat history', history => {
+    messages.innerHTML = ''
+    for (const message of history) {
+        showMessage(message)
+    }
+})
+
+socket.on('error', error => {
+    alert(error)
+})
+
+
+function showMessage(message) {
+    function addElement(template, msg) {
+        const el = template.content.cloneNode(true).querySelector('.message')
+        if (message.user === socket.id) {
+            el.classList.add('own-message')
+        }
+        el.querySelector('.username').innerText = message.userName
+        if (msg !== null && msg !== undefined) {
+            el.querySelector('.message-text').innerText = msg
+        }
+        messages.appendChild(el)
+    }
+
+    switch (message.type) {
+        case 'text':
+            addElement(chatMessageTemplate, message.message)
+            break
+
+        case 'joined':
+            addElement(userJoinedTemplate)
+            break
+
+        case 'left': 
+            addElement(userLeftTemplate)
+            break
+    }
+}
+
+
+newChatButton.onclick = () => {
+    newRoomPopup.classList.remove('hidden')
+    roomNameTf.focus()
+}
+
+joinChatButton.onclick = () => {
+    joinRoomPopup.classList.remove('hidden')
+    roomCodeTf.focus()
+}
+
+
+createRoomForm.onsubmit = e => {
+    e.preventDefault()
+    socket.emit('create room', roomNameTf.value)
+    roomNameTf.value = ''
+    closePopup(newRoomPopup)
+}
+
+
+joinRoomForm.onsubmit = e => {
+    e.preventDefault()
+    socket.emit('join room', roomCodeTf.value)
+    roomCodeTf.value = ''
+    closePopup(joinRoomPopup)
+}
+
+
+function closePopup(popup) {
+    popup.classList.add('hidden')
+}
+
+
+
+function openRoom(roomButton, roomCode) {
+    if (roomButton === activeButton) { return }
+
+    if (activeButton) {
+        activeButton.classList.remove('active')
+    }
+    activeButton = roomButton
+    roomButton.classList.add('active')
+    chatNotSelectedView.classList.add('hidden')
+    chatSelectedView.classList.remove('hidden')
+
+    socket.emit('open room', roomCode)
+}
+
 
 
 function animateSendIcon() {
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const io = new Server(server)
 
 const fs = require('fs')
 
+const randomise = require('randomatic')
+
 fs.readdirSync('Client').forEach(file => {
     if (file === 'index.html') {
         app.get('/', (req, res) => {
@@ -21,15 +23,135 @@ fs.readdirSync('Client').forEach(file => {
 })
 
 
-io.on('connection', socket => {
-    console.log(`a 🧑‍💻 connected`)
+const rooms = {
+    // Room code: Room name
+}
 
+const names = {
+    // Socket id: Name
+}
+
+const currentRooms = {
+    // Socket id: Room code
+}
+
+const messages = {
+    // Room code: [Message]
+}
+
+
+function generateCode(name) {
+    const code = randomise('A0', 4)
+    if (!rooms[code]) { 
+        rooms[code] = name
+        return code
+    }
+    return generateCode()
+}
+
+
+function sendMessage(socket, msg) {
+    const roomCode = currentRooms[socket.id]
+    if (roomCode === null || roomCode === undefined) { return }
+    if (!(roomCode in messages)) { messages[roomCode] = [] }
+    messages[roomCode].push(msg)
+    console.log(messages)
+    io.to(roomCode).emit('chat message', msg)
+}
+
+
+io.on('connection', socket => {
     socket.on('disconnect', () => {
-        console.log(`🧑‍💻 disconnected`)
+        const curr = currentRooms[socket.id]
+        if (curr !== null && curr !== undefined) {
+            socket.leave(curr)
+        }
+        
+        sendMessage(socket, {
+            user: socket.id,
+            userName: names[socket.id],
+            type: 'left'
+        })
+
+        delete names[socket.id]
+        delete rooms[socket.id]
+        delete currentRooms[socket.id]
+    })
+
+    socket.on('register', name => {
+        names[socket.id] = name
+        console.log(names)
     })
 
     socket.on('chat message', msg => {
-        io.emit('chat message', msg)
+        const msgObj = {
+            user: socket.id,
+            userName: names[socket.id],
+            type: 'text',
+            message: msg
+        }
+
+        sendMessage(socket, msgObj)
+    })
+
+    socket.on('create room', name => {
+        const code = generateCode(name)
+        socket.join(code)
+        socket.emit('room joined', `${code}#${name}`)
+    })
+
+    socket.on('join room', code => {
+        if (code.length !== 4) {
+            socket.emit('error', 'Invalid code format: Room codes are always a combination of 4 alphanumeric characters.')
+            return
+        }
+
+        const name = rooms[code.toUpperCase()]
+        if (name) {
+            socket.join(code)
+            socket.emit('room joined', `${code}#${name}`)
+        } else {
+            socket.emit('error', `Room with code ${code} doesn't exist`)
+        }
+    })
+
+    socket.on('open room', code => {
+        const curr = currentRooms[socket.id]
+        if (curr !== null && curr !== undefined) {
+            socket.leave(curr)
+        }
+
+        sendMessage(socket, {
+            user: socket.id,
+            userName: names[socket.id],
+            type: 'left'
+        })
+
+        currentRooms[socket.id] = code
+
+        sendMessage(socket, {
+            user: socket.id,
+            userName: names[socket.id],
+            type: 'joined'
+        })
+
+        socket.join(code)
+
+        socket.emit('opened room', {
+            roomCode: code,
+            roomName: rooms[code],
+            messages: []
+        })
+    })
+
+
+    socket.on('get chat history', code => {
+        if (code !== currentRooms[socket.id]) {
+            socket.emit('error', 'Trying to access the history of a channel you haven\'t joined.')
+            return
+        }
+
+        socket.emit('chat history', messages[code])
     })
 })
 
